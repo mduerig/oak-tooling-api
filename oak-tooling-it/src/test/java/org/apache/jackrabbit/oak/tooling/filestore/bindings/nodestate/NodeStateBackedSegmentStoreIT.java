62dfbca452ea287b05f9774cad8fef91820b7652
mduerig <mduerig@apache.org>
2018-04-11 13:48:50 +0200

    Remove workaround for OAK-7398

--- oak-tooling-it/src/test/java/org/apache/jackrabbit/oak/tooling/filestore/bindings/nodestate/NodeStateBackedSegmentStoreIT.java
+++ oak-tooling-it/src/test/java/org/apache/jackrabbit/oak/tooling/filestore/bindings/nodestate/NodeStateBackedSegmentStoreIT.java
@@ -108,12 +108,9 @@ public class NodeStateBackedSegmentStoreIT {
         assumeFalse("Cannot run with empty segment store", isEmpty(tars));
 
         tars.forEach(tar -> {
-            // michid FIXME OAK-7398 Only tars should be returned here
-            if (tar.name().startsWith("data")) {
-                assertTrue(tar.name().startsWith("data"));
-                assertTrue(tar.size() > 0);
-                assertTrue(tar.segments().iterator().hasNext());
-            }
+            assertTrue(tar.name().startsWith("data"));
+            assertTrue(tar.size() > 0);
+            assertTrue(tar.segments().iterator().hasNext());
         });
     }
 
